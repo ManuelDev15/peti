946e8ab77c6149e22ef8c92ccf8b9883940eac22
ManuelDev15 <manuelfernandezmen99@gmail.com>
2024-10-26 11:21:28 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -21,6 +21,36 @@ def delete_message(chat_id, message_id, delay):
     bot.delete_message(chat_id, message_id)
 
 
+################
+
+
+@bot.message_handler(func=lambda message: message.chat.type == 'group')
+def handle_message(message):
+    if '#' in message.text:
+        if '#peticion' in message.text:
+            if message.from_user.username is not None:
+                username = message.from_user.username
+                msgo = message.text[10:]
+                msgn = f'<code>{msgo}</code>\n\n<b>✅Petición de:</b> @{username}'
+                save = f"<b>Petición archivada📦</b>"
+                bot.send_message(idgroup, msgn, parse_mode='HTML')
+                bot.reply_to(message, save, parse_mode='HTML')
+            else:
+                ID = message.from_user.id
+                msgo = message.text[10:]
+                msgn = f'<code>{msgo}</code>\n\n<b>✅Petición de:</b> <a href="tg://openmessage?user_id={ID}">ID:{ID}</a>'
+                bot.send_message(idgroup, msgn, parse_mode='HTML')
+        else:
+            ms = "<b>☝🏻🤓Las peticiones son de esta forma:</b>\n\n<code>#peticion *y aquí inserta la petición*</code>\n\n<i>•Solo así se guardará en el bot•</i>"
+
+            try:
+                eli = bot.reply_to(message, ms, parse_mode='HTML')
+                threading.Thread(target=delete_message, args=(message.chat.id, eli.message_id, 15)).start()
+            except Exception as e:
+                print(f"Error al enviar mensaje informativo:\n{e}")
+
+###############
+
 @bot.message_handler(commands=['reset'])
 def resetarchiving(message):
     archived_messages.clear()
@@ -63,33 +93,7 @@ def send_archived_messages(message):
         bot.reply_to(message, "<i>No hay mensajes archivados</i>", parse_mode='HTML' )
 
 
-################
 
-
-@bot.message_handler(func=lambda message: message.chat.type == 'group')
-def handle_message(message):
-    if '#' in message.text:
-        if '#peticion' in message.text:
-            if message.from_user.username is not None:
-                username = message.from_user.username
-                msgo = message.text[10:]
-                msgn = f'<code>{msgo}</code>\n\n<b>✅Petición de:</b> @{username}'
-                save = f"<b>Petición archivada📦</b>"
-                bot.send_message(idgroup, msgn, parse_mode='HTML')
-                bot.reply_to(message, save, parse_mode='HTML')
-            else:
-                ID = message.from_user.id
-                msgo = message.text[10:]
-                msgn = f'<code>{msgo}</code>\n\n<b>✅Petición de:</b> <a href="tg://openmessage?user_id={ID}">ID:{ID}</a>'
-                bot.send_message(idgroup, msgn, parse_mode='HTML')
-        else:
-            ms = "<b>☝🏻🤓Las peticiones son de esta forma:</b>\n\n<code>#peticion *y aquí inserta la petición*</code>\n\n<i>•Solo así se guardará en el bot•</i>"
-
-            try:
-                eli = bot.reply_to(message, ms, parse_mode='HTML')
-                threading.Thread(target=delete_message, args=(message.chat.id, eli.message_id, 15)).start()
-            except Exception as e:
-                print(f"Error al enviar mensaje informativo:\n{e}")
 
 ### MAIN #######################
 def run_server():
