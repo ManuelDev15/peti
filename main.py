e80e54d98f40c40aee7cb94fb1ebc49b5e235a29
ManuelDev15 <manuelfernandezmen99@gmail.com>
2024-10-26 00:43:01 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -10,11 +10,63 @@ API_TOKEN = '7764656259:AAF_7mPJUHp7egPMxnINjk0FMjgyu4q8Rbs'
 bot = telebot.TeleBot(API_TOKEN)
 idgroup = -1002369751844
 
+channel = -1002360088103
+chann = "DevFast_FreeUp"
+
+admins = {7346891727, 6181692448}
+archived_messages = []
+
 def delete_message(chat_id, message_id, delay):
     time.sleep(delay)
     bot.delete_message(chat_id, message_id)
 
-@bot.message_handler(content_types=['text'])
+
+@bot.message_handler(commands=['reset'])
+def resetarchiving(message):
+    archived_messages.clear()
+    bot.reply_to(message, "<b><i>Reset ejecutado</i></b>", parse_mode='HTML')
+
+@bot.message_handler(func=lambda message: True and not message.text.startswith('/') and message.chat.type == 'private')
+def archive_message(message):
+    user_id = message.from_user.id
+    if user_id not in admins:
+        return
+    if message.chat.type == 'private':
+        if archived_messages is not None:
+            args = message.text.split("=")
+            if len(args) == 2:
+                arg0 = args[0]
+                arg1 = args[1]
+                forms = f"<a href='{arg1}'>{arg0} ⬅</a>"
+                archived_messages.append("• " + f"<b>{forms}</b>")
+                cmessage = "<b>📄Lista de peticiones subidas:</b>\n\n" + "\n".join(archived_messages) + "\n\n<b>/listo</b>"
+                bot.reply_to(message, cmessage, parse_mode='HTML', disable_web_page_preview=True)
+
+            else:
+                bot.reply_to(message, "<b>Recuerda enviar el nombre, el signo igual (=) y después el link</b>", parse_mode='HTML')
+
+
+
+@bot.message_handler(commands=['listo'])
+def send_archived_messages(message):
+    if archived_messages:
+        combined_message = "<b>📄Lista de peticiones subidas:</b>\n\n" + "\n".join(archived_messages)
+        msl = bot.send_message(channel, combined_message, parse_mode='HTML', disable_web_page_preview=True)
+        
+        meslink = f"https://t.me/{chann}/{msl.message_id}"
+        linf = f"<a href='{meslink}'>Mensaje enviado al canal☑️</a>"
+        listo = f"\n\n<b>{linf}</b>"
+        bot.send_message(message.chat.id, listo, parse_mode='HTML', disable_web_page_preview=True)
+        
+        archived_messages.clear()
+    else:
+        bot.reply_to(message, "<i>No hay mensajes archivados</i>", parse_mode='HTML' )
+
+
+################
+
+
+@bot.message_handler(func=lambda message: message.chat.type == 'group')
 def handle_message(message):
     if '#' in message.text:
         if '#peticion' in message.text:
